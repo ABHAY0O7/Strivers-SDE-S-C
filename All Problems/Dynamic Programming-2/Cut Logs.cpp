5c2d81830dc6c5aa94329a1d3115c780c7c3bd28
Abhay Singh <abhaysingh936851@gmail.com>
2023-06-25 16:33:32 +0530

    Completed Dynamic Programming-2 Cut Logs

---
+++ All Problems/Dynamic Programming-2/Cut Logs.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+
+int cutLogs(int k, int n)
+{
+    // Write your code
+
+    // function<int(int, int, int)> dfs = [&] (int l, int r, int axes) -> int {
+    //     if(l == r) return 0;
+    //     if(axes == 1) return r - l;
+
+    //     int ans = 1e8;
+    //     int i = (r + l - 1) / 2;
+    //     // for(int i = l; i < r; i++) {
+    //         int take = dfs(i + 1, r, axes);
+    //         int nottake = dfs(l, i, axes-1);
+    //         ans = min(ans, 1 + max(take, nottake));
+    //     // }
+    //     return ans;
+    // };
+    vector<vector<int>> dp(n + 1, vector<int> (k + 1, -1));
+    function<int(int, int)> dfs = [&] (int idx, int axes) -> int {
+        if(axes == 1) return idx;
+        if(idx <= 1) return idx;
+
+        if(dp[idx][axes] != -1) return dp[idx][axes];
+
+        
+        int ans = 1e8;
+
+        int l = 1, r = idx;
+        while(l <= r) {
+            int i = (l + r + 1) / 2;
+            int curr1 = 1 + dfs(i - 1, axes - 1);
+            int curr2 = 1 + dfs(idx - i, axes);
+
+            ans = min(ans, max(curr1, curr2));
+
+            if(curr1 <= curr2) l = i + 1;
+            else r = i - 1; 
+        }
+        
+
+        // int ans2 = 1 + dfs(nidx - 1, axes);
+        // ans2 = max(ans, 1 + dfs(idx - nidx, axes - 1));
+        // return dp[idx][axes] = min(ans, ans2);
+        return dp[idx][axes] = ans;
+    };
+
+    return dfs(n, k);
+}
+
