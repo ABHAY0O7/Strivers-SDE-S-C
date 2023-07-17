f22208aaea8a680c1a4a87f845ab77944793186b
Abhay Singh <abhaysingh936851@gmail.com>
2023-07-17 19:17:57 +0530

    Completed Stack and Queue Valid Parentheses

---
+++ All Problems/Stack and Queue/Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+bool isValidParenthesis(string expression)
+{
+    int n = expression.size();
+    stack<char> s;
+    for (int i = 0; i < n; i++)
+    {
+        char ch = expression[i];
+
+        if (ch == '(' || ch == '{' || ch == '[')
+            s.push(ch);
+
+        else
+        {
+            // for closing bracket
+            if (!s.empty())
+            {
+                char top = s.top();
+
+                if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '['))
+                    s.pop();
+
+                else
+                    return false;
+            }
+
+            else
+                return false;
+        }
+    }
+
+    if (s.empty())
+        return true;
+
+    else
+        return false;
+}
+
